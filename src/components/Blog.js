d5639ab5c34da0294619db4da9399b6ffcdea2b3
Marek Elmayan <elmayan.marek@gmail.com>
2022-12-06 09:45:06 +0200

    Ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,30 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  console.log(blog);
+  const [visible, setVisible] = useState(false);
+  return (
+    <div style={{ border: "1px solid black", margin: "5px", padding: 2 }}>
+      <b>{blog.title}</b> by {blog.author}
+      {visible && (
+        <>
+          <div>
+            <a href={blog.url}>{blog.url}</a>
+          </div>
+          <div>
+            Likes : {blog.likes}
+            <button>+1</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </>
+      )}
+      {visible ? (
+        <button onClick={() => setVisible(false)}>Hide</button>
+      ) : (
+        <button onClick={() => setVisible(true)}>View</button>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
