b8bef80c0b2ef18a2251b5d08e55d97ebdbb1cc2
Marek Elmayan <elmayan.marek@gmail.com>
2022-12-07 11:56:03 +0200

    Ex 5.18

--- cypress/e2e/blogList.cy.js
+++ cypress/e2e/blogList.cy.js
@@ -1,11 +1,37 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('Blog App')
     cy.contains('Log in to application')
+  })
+
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Matti Luukkainen logged-in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('admin')
+      cy.get('#password').type('admin')
+      cy.get('#login-button').click()
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged-in')
+    })
   })
 })
